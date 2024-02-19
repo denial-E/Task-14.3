723109691b8dd718a1cb535a8fec38fd3d4b622f
denial-E <shempeter221992@gmail.com>
2024-02-20 01:14:33 +0530

    run bro

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dataContainer = document.getElementById('data-container');
+  
+   
+    function fetchData(url) {
+      return new Promise((resolve, reject) => {
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => resolve(data))
+          .catch(error => reject(error));
+      });
+    }
+  
+   
+    const apiUrl = 'https://bible-api.com/romans+12:1-2';
+  
+    
+    fetchData(apiUrl)
+      .then(data => {
+      
+        dataContainer.textContent = JSON.stringify(data);
+      })
+      .catch(error => {
+        
+        console.error('Error fetching data:', error);
+      });
+  });
+  
